16fde844b885c21b6f1bf9af2df43bf94eb1df76
Jônathasds <jonathadelgado@gmail.com>
2022-06-01 07:45:29 -0300

    Calculadora

---
+++ javacript.js
@@ -0,0 +1,27 @@
+const resultado = document.querySelector( '.result' );
+const confirmar = document.querySelector( 'igual' ) ;
+
+function insert( valor){
+    resultado.innerHTML += valor;
+}
+
+function clean(){
+    resultado.innerHTML = ' ';
+}
+
+function backspace(){
+    if( resultado.textContent ){
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length-1);
+    }
+}
+
+
+function calcular() {
+    if( resultado.textContent ) {
+        document.getElementById("resultado").innerHTML = eval(resultado.innerHTML)
+    }
+}
+    
+
+
